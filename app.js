97f8898861f6d18f8cef66ae45a0cec2f7be747d
Jem Young <jemuel.young@gmail.com>
2016-11-12 00:32:34 -0600

    Update app

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
- const express = require("express");
- const app = express();
- 
- app.get("/", function(req, res) {
-    res.sendFile( __dirname + '/index.html');
- });
- 
- 
- const port = 3001;
- app.listen(port, function() {
-   console.log("Listening on " + port);
- });
+'use strict';
+
+const express = require("express");
+const app = express();
+
+// Static css/js files
+app.use('/static', express.static('./dist'));
+
+app.get("/", function(req, res) {
+  res.sendFile( __dirname + '/index.html');
+});
+
+
+const port = 3001;
+
+// Start server
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
